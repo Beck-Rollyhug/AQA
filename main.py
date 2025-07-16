539ed1ec864e71eeb45ecc8e6d6f25ca06f686d2
Лыжин Алексей Вячеславович <aleksey.lyzhin@rt.ru>
2025-07-16 23:39:02 +0500

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from practice1.basic_info import task1
+from practice1.user_profile import task2
+from practice1.test_case_stats import task3
+from practice1.bug_reports import task4
+from practice1.bug_details import task5
+
+from practice2.functions_intro import task6
+from practice2.age_message import task7
+from practice2.number_sum import task8
+from practice2.triangle_check import task9
+from practice2.nested_functions import task10
+
+task10()
